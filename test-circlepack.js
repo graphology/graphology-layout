f4e9d4a47d2862de9d3cf847a71d6c46ceddadc2
Nicolas Aspert <nicolas.aspert@epfl.ch>
2020-05-18 11:30:51 +0200

    - use closeTo to verify final layout positions

--- test-circlepack.js
+++ test-circlepack.js
@@ -1,27 +1,36 @@
-var assert = require('assert'),
+var assert = require('chai').assert,
     Graph = require('graphology'),
     circlepack = require('./circlepack.js');
 
 
+function closeTo(A, B) {
+    assert.closeTo(A, B, 1e-6);
+}
+
+// Check if positions are close up to the specified precision in closeTo
+function checkPositions(A, B) {
+    Object.keys(A).forEach(function (key) {
+        closeTo(A[key].x, B[key].x);
+        closeTo(A[key].y, B[key].y);
+    });
+}
+
 describe('circlepack-layout', function () {
     it('should correctly produce a layout.', function() {
         var graph = new Graph();
-        [1, 2, 3, 4].forEach(function(node) {
+        [1, 2, 3, 4].forEach(function (node) {
             graph.addNode(node);
             graph.setNodeAttribute(node, 'degree', 0);
             graph.setNodeAttribute(node, 'size', 10);
         });
 
         var positions = circlepack(graph, {});
-
-        assert.deepEqual(positions,
-            {
-                1: {x: -10, y: 0},
-                2: {x: 10, y: 0},
-                3: {x: 0, y: 17.320508075688772},
-                4: {x: 0, y: -17.320508075688772}
-            });
-
+        checkPositions(positions, {
+            1: {x: -10, y: 0},
+            2: {x: 10, y: 0},
+            3: {x: 0, y: 17.320508075688772},
+            4: {x: 0, y: -17.320508075688772}
+        });
     });
 
     it('should be possible to assign the results to the nodes.', function() {
@@ -34,15 +43,13 @@ describe('circlepack-layout', function () {
 
         circlepack.assign(graph, {});
 
-        assert.deepEqual(
-            {1: get(1), 2: get(2), 3: get(3), 4: get(4)},
+        checkPositions({1: get(1), 2: get(2), 3: get(3), 4: get(4)},
             {
                 1: {x: -1, y: 0},
                 2: {x: 1, y: 0},
                 3: {x: 0, y: 1.7320508075688772},
                 4: {x: 0, y: -1.7320508075688772}
-            }
-        );
+            });
     });
 
     it('should be possible to offset the center.', function() {
@@ -53,15 +60,13 @@ describe('circlepack-layout', function () {
 
         var positions = circlepack(graph, {center: 0.7});
 
-        assert.deepEqual(
-            positions,
+        checkPositions(positions,
             {
-                1: {x: -1 + 0.7, y: 0.7},
+                1: {x: -0.3, y: 0.7},
                 2: {x: 1.7, y: 0.7},
-                3: {x: 0.7, y: 1.7320508075688772 + 0.7},
-                4: {x: 0.7, y: -1.7320508075688772 + 0.7}
-            }
-        );
+                3: {x: 0.7, y: 2.4320508075688772},
+                4: {x: 0.7, y: -1.0320508075688772}
+            });
     });
 
     // The tests results below have been checked with Gephi.
@@ -75,15 +80,12 @@ describe('circlepack-layout', function () {
 
         var positions = circlepack(graph, {hierarchyAttributes: ['degree']});
 
-        assert.deepEqual(
-            positions,
-            {
-                1: {x: 21.547005383792513, y: 0},
-                2: {x: -40, y: -5.773502691896258},
-                3: {x: -20, y: -5.773502691896258},
-                4: {x: -30, y: 11.547005383792513}
-            }
-        );
+        checkPositions(positions, {
+            1: {x: 21.547005383792513, y: 0},
+            2: {x: -40, y: -5.773502691896258},
+            3: {x: -20, y: -5.773502691896258},
+            4: {x: -30, y: 11.547005383792513}
+        });
     });
 
     it('should produce layout according to properties (2/2).', function() {
@@ -97,17 +99,15 @@ describe('circlepack-layout', function () {
 
         var positions = circlepack(graph, {hierarchyAttributes: ['community', 'degree']});
 
-        assert.deepEqual(
-            positions,
-            {
-                1: {x: -30, y: 0},
-                2: {x: -90, y: 0},
-                3: {x: -70, y: 0},
-                4: {x: 70, y: 0},
-                5: {x: 10, y: 0},
-                6: {x: 30, y: 0}
-            }
-        );
+        checkPositions(positions, {
+            1: {x: -30, y: 0},
+            2: {x: -90, y: 0},
+            3: {x: -70, y: 0},
+            4: {x: 70, y: 0},
+            5: {x: 10, y: 0},
+            6: {x: 30, y: 0}
+        });
+
     });
 
     it('should handle an non-existing attribute.', function() {
@@ -118,7 +118,8 @@ describe('circlepack-layout', function () {
         });
 
         var positions = circlepack(graph, {hierarchyAttributes: ['degree', 'dummy']});
-        assert.deepEqual(positions,
+
+        checkPositions(positions,
             {
                 1: {x: -10, y: 0},
                 2: {x: 10, y: 0},
