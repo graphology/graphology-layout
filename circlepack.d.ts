9900a48c464e27624e48c7bceabf057d0bcb5b5f
Yomguithereal <guillaumeplique@gmail.com>
2020-05-25 15:08:47 +0200

    Consistent indent

--- circlepack.d.ts
+++ circlepack.d.ts
@@ -3,21 +3,21 @@ import Graph from 'graphology-types';
 type RNGFunction =  () => number;
 
 export type CirclePackLayoutOptions = {
-    attributes: {
-        x: string,
-        y: string
-    },
-    center: number,
-    hierarchyAttributes: string[],
-    rng: RNGFunction,
-    scale: number
+  attributes: {
+    x: string,
+    y: string
+  },
+  center: number,
+  hierarchyAttributes: string[],
+  rng: RNGFunction,
+  scale: number
 };
 
 type LayoutMapping = {[key: string]: {x: number, y: number}};
 
 interface ICirclePackLayout {
-    (graph: Graph, options?: CirclePackLayoutOptions): LayoutMapping;
-    assign(graph: Graph, options?: CirclePackLayoutOptions): void;
+  (graph: Graph, options?: CirclePackLayoutOptions): LayoutMapping;
+  assign(graph: Graph, options?: CirclePackLayoutOptions): void;
 }
 
 declare const circlepack: ICirclePackLayout;
